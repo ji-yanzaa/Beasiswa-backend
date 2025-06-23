c8509a00b9bd7f71054ff8e370fee2529c91e681
[ji-yanzaa] <[ryanzaazaa@gmail.com]>
2025-06-23 10:15:00 +0700

    push kembali semua perubahan

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 
 const authRoutes = require('./routes/authRoutes');
 const scholarshipRoutes = require('./routes/scholarshipRoutes');
-const bookmarksRoutes = require('./routes/bookmarkRoutes')
+const bookmarkRoutes = require('./routes/bookmarkRoutes');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -17,6 +17,7 @@ app.use(express.json());
 // Routing utama
 app.use('/api/auth', authRoutes);
 app.use('/api/scholarships', scholarshipRoutes);
+app.use('/api/bookmarks', bookmarkRoutes);
 
 // Route default
 app.get('/', (req, res) => {
--- routes/bookmarkRoutes.js
+++ routes/bookmarkRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const bookmarkController = require('../controllers/bookmarkController');
+const bookmarkController = require('../controllers/bookmarkcontroller');
 const { verifyToken } = require('../middleware/authMiddleware');
 
 // hanya user yang login bisa akses
